822e4784fdacc400c121b4b20122d89f8f1d6bc0
Nathan Wichman <wichmann@eos19.cis.gvsu.edu>
2018-09-25 12:22:14 -0400

    Second Commit

--- SWclient.java
+++ SWclient.java
@@ -5,10 +5,17 @@ import java.nio.channels.*;
 import java.util.*;
 import java.nio.file.Path;
 import java.nio.file.Files;
+/*************************************************************************************
+UDP client using Datagram Sockets. Requests the Server for a file, then recieves
+said file via the sliding window model.
 
+@Author Nathan Wichman
+@Version september 2018
+*************************************************************************************/
 class udpclient{
     public static void main(String args[]){
 	try{
+	    /** Receiving user input for port number and iPadress **/
 	    Scanner scnr = new Scanner(System.in);
 	    System.out.println("Enter a port number: ");
 	    int portNumber = scnr.nextInt();
@@ -17,19 +24,21 @@ class udpclient{
 	    String ipAddress = scnr.nextLine();
 	    System.out.println("IpReceived: " + ipAddress);
 	    
-	    //Datagram socket. Even though it doesnt have the word socket in it
-	    //Remember a DatagramChannel is a Datagram socket
+	    /** Creating a datagram channel **/
 	    DatagramChannel sc = DatagramChannel.open();
 	    Console cons = System.console();
+
+	    /** Sending the file name to the inputted ip and port number **/
 	    String fileName  = cons.readLine("Enter a File Name: ");
 	    ByteBuffer buf = ByteBuffer.wrap(fileName.getBytes());
 	    sc.send(buf, new InetSocketAddress((ipAddress),portNumber));
             
+	    /** Just for debugging purposes, sending a 1 to check connection **/
 	    String example = "1";
 	    ByteBuffer buffer = ByteBuffer.wrap(example.getBytes());
 	    sc.send(buffer, new InetSocketAddress((ipAddress),portNumber));
 
-
+	    /** Just for debugging as well, receiving a "check" string from the server **/
 	    ByteBuffer buf3 = ByteBuffer.allocate(5000);
 	    sc.receive(buf3);
 	    buf3.flip();
@@ -43,72 +52,59 @@ class udpclient{
 		    System.out.println("Test Failed");
 	    }
 	    
-
+	    /** Setting up the output file to write too **/
 	    File outputFile = new File("output.txt");
 	    FileOutputStream outStream = new FileOutputStream("output.txt");
 	    
-
+	    /** Byte array to hold the received data **/
 	    byte[] receivedData = {};
-	    byte[] temp;
+	  //  byte[] temp;
 	    int acknowledgment = 0;
 	    while(true){
+		    /** Receiving packets from Server **/
 		    ByteBuffer buf2 = ByteBuffer.allocate(5000);
 		    sc.receive(buf2);
 		    buf2.flip();
 		    byte[] a = new byte[buf2.remaining()];
+		    /** Converting to string to test for termination code **/
 		    String test = new String(buf2.array());
+		    /** Removing null charachters from string **/
 		    test = test.replaceAll("\0+$", "");
 		    buf2.get(a);
 		    if(test == null){
 			    System.out.println("null");
 		    }
-		    if(test.equals("done")){
+		    else  if(test.equals("done")){
 			    System.out.println("done");
 			    break;
-		    }else{
-		//	    System.out.println("test = " + test);
 		    }
 
+		    /** Adding new packet to the total byte array (ReceivedData[]) **/
 		    byte[] combo = new byte[a.length + receivedData.length];
-		    
 		    System.arraycopy(receivedData, 0, combo,0, receivedData.length);
 		    System.arraycopy(a, 0, combo, receivedData.length, a.length);
-
 		    receivedData = combo;
-	//	    outStream.write(receivedData);
 		    System.out.println("Bytes Received: " + a);
 		    
 		    
-	  	    String ack = "c" + Integer.toString(acknowledgment);
+		    /** Sending acknoledgment **/
+	  	    String ack = "c" + Integer.toString(acknowledgment); //Every acknowledgment begins with c
+		    //I had issues checking if the string was null on the server end, so instead I check that
+		    //it begins with c then remove the c leaving just the integer on the server end.
 		    ByteBuffer buf4 = ByteBuffer.wrap(ack.getBytes());
 		    sc.send(buf4, new InetSocketAddress((ipAddress), portNumber));
 		    acknowledgment++;
 
 	    }
 
-    	    
+    	    /** Writing File **/
 	    if(receivedData != null){
 	  	  outStream.write(receivedData);
 	    }else{
 		    System.out.println("ReceivedData was empty");
 	    } 
 
-/*	    ByteBuffer buf2 = ByteBuffer.allocate(5000);
-	    sc.receive(buf2);
-	    buf2.flip();
-	    byte[] a = new byte[buf2.remaining()];
-	    buf2.get(a);
-	    if(a != null){
-		File outputFile = new File("output.txt");
-		FileOutputStream outStream = new FileOutputStream("output.txt");
-		outStream.write(a);
-	    } */
-
-/*
-	   // String mes = "1"; /*
-	    String mes = "1";
-	    ByteBuffer buf3 = ByteBuffer.wrap(mes.getBytes());
-	    sc.send(buf3, new InetSocketAddress((ipAddress), portNumber)); */
+	    /** Closing Datagram Socket Channel **/
 	    sc.close();
 
 	}catch(IOException e){
--- SWserver.java
+++ SWserver.java
@@ -10,28 +10,37 @@ import java.io.FileInputStream;
 import java.io.File;
 import java.util.Scanner;
 
+/************************************************************************
+UDP server using Datagram Sockets. It recieves a file name from the client
+and then proceeds to send that file to the client via the sliding window
+model.
+
+@author Nathan Wichman
+@version September 2018
+*************************************************************************/
 class udpserver{
     public static void main(String args[]){
 	try{
+	    /** Creating an object of this class to use private helper methods **/
 	    udpserver manager = new udpserver();
+
+	    /** Recieving the port number from the user **/
 	    Scanner scnr = new Scanner(System.in);
 	    System.out.println("Enter a port number");
 	    int portNumber = scnr.nextInt();
 	    
+	    /**Setting up the datagram channel and selectore **/
 	    DatagramChannel c = DatagramChannel.open();
 	    Selector s = Selector.open();
 	    c.configureBlocking(false);
 	    c.register(s,SelectionKey.OP_READ);
 	    c.bind(new InetSocketAddress(portNumber));
 
+	    /** Declaring file and Buffer reader variabler **/
 	    File file;
 	    BufferedReader reader;
 
 	    while(true){
-		/*select checks the channels to see
-		//if we can do the operations.
-		//it returns the numkber or channels
-		//that we can do that operation on */
 		int n = s.select(5000);
 		if(n == 0){
 		    System.out.println("got a timeout");
@@ -39,6 +48,8 @@ class udpserver{
 		    Iterator i = s.selectedKeys().iterator();
 		    while(i.hasNext()){
 			SelectionKey k = (SelectionKey)i.next();
+
+			/** Receiving file name from client **/
 			ByteBuffer buf = ByteBuffer.allocate(4096);
 			SocketAddress clientaddr = c.receive(buf);
 			String fileName  = new String(buf.array());
@@ -52,98 +63,97 @@ class udpserver{
 		        file = new File(fileName);
 			if(file.exists() && !file.isDirectory()){
 		            	reader = new BufferedReader(new FileReader(fileName));
-			}
-			else{
-				System.out.println("File Not Found");
-			}
-
+			
+			/** Testing if Acknowledgments can be gotten from the client (for debugging) **/
 			ByteBuffer buffer2 = ByteBuffer.allocate(4096);
 			c.receive(buffer2);
 			String example = new String(buffer2.array());
-			System.out.println("Example is: " + example);
+			System.out.println("Recieved From Client: " + example);
 
+			/** Reading in the file into a byte array **/
 			byte[] fileContent = Files.readAllBytes(file.toPath());
 			
+			/** Testing connection with the Client (For Debugging) **/
 			byte[] test = "check".getBytes();
 			ByteBuffer buf2 = ByteBuffer.wrap(test);
 			c.send(buf2, clientaddr);
 
+			/** Getting File Size **/
 			int size = fileContent.length;
 			System.out.println("Size of file: " + size);
 
+			/** Finding the number of packets the Server needs to send, with a max size of 1024 bytes **/
 			int numPackets = (size / 1024) + 1;
 			
+			/** Holds the acknoledgments received **/
 			ArrayList<Integer> acks = new ArrayList<Integer>();
+
+			/** Determins if the Server can send to the client **/
 			boolean canSend = true;
+
+			/** counting variable for the loop **/
 			int counter = 0;
+
+			/** Upper and lower limits of the Sliding Window **/
 			int upperLim = 4;
 			int lowerLim = 0;
 
 			for(int j = 0; j < numPackets;  j++){
 				if(canSend == true){
+					/** Sending packet **/
 					ByteBuffer buffer = ByteBuffer.allocate(4096);
 					byte[] packet = Arrays.copyOfRange(fileContent, (j*1024), ((j+1)*1024));
 					manager.sendPacket(c, packet, clientaddr); 
 				}
 				
+				/** Receiving Acknowledgment **/
 				ByteBuffer ackBuf = ByteBuffer.allocate(4096);
 				c.receive(ackBuf);
 				String ack = new String(ackBuf.array());
+
+				/** First letter of Acknowledment is always c **/
 				if(ack.charAt(0) == 'c'){
+					/** Removing the 'c' leaving just an integer in the string **/
 					ack = ack.substring(1);
 					System.out.println("Acknowledgment Received" + ack);
+
+					/** removing unneccecary null charachters **/
 					ack = ack.replaceAll("\0+","");
+
+					/** converting into an integer **/
 					acks.add(Integer.parseInt(ack));
 					canSend = true;
 				}
+
+				/** Checking to see if we have reached the sliding window limit **/
 				if(counter >= upperLim){
 					if(!acks.contains(lowerLim)){
-						canSend = false;
-						j--;
+						canSend = false; //if reached, sending stops
+						j--; //repeating this iteration of the for loop
 						counter--;
 						System.out.println("Sliding Window limit reached, pausing for acknowledment");
 					}
 					else{
+						/** If the lower limit is in the arrayList, we can adjust the window and continue
+						 * sending **/
 						lowerLim++;
 						upperLim++;
 						System.out.println("Sliding Window up by 1");
 					}
 			        }
 				counter++;
-				System.out.println(acks);
-				/*if(counter >= upperLim){
-					if(!acks.contains(lowerLim)){
-						canSend = false;
-						j--;
-						counter--;
-						System.out.println("Sliding Window Limit Reached, pausing for Acknowledment");
-					}
-					else{
-						lowerLim++;
-						upperLim++;
-						System.out.println("Sliding Window up by 1");
-					}
-				}
-				else{
-					canSend = true;
-				}*/
-				
 			}
 			
-		
+			/** Sending Termination Code to Client so it knows to end its receiving
+			 * and compile the file **/
 			byte[] termination = "done".getBytes();
 			ByteBuffer buf3 = ByteBuffer.wrap(termination);
 			c.send(buf3, clientaddr);
-		//	manager.sendPacket(c, termination, clientaddr);
-
-			
-			//manager.sendPacket(c, fileContent, clientaddr);
 		        
-	
-			
 			System.out.println("Finished");
-			c.send(buf3, clientaddr);
-
+			}else{
+				System.out.println("File Not Found");
+			}
 		    }
 		}
 	    }
@@ -152,7 +162,13 @@ class udpserver{
 	}
     }
 
-    private void sendPacket(DatagramChannel c, byte[] packet, SocketAddress clientaddr) {
+/************************************************************************************************
+Private helper method to send a packet to the client.
+
+@param A datagramChannel, a packet to send, and the clients socket address
+@returns void
+************************************************************************************************/
+ private void sendPacket(DatagramChannel c, byte[] packet, SocketAddress clientaddr) {
 	    try{
 		ByteBuffer buf = ByteBuffer.wrap(packet);
 		c.send(buf, clientaddr);
@@ -160,31 +176,4 @@ class udpserver{
 		    System.out.println("Error Sending Packet");
 	    }
     }
-
-    private int receiveAck(String message){
-	    switch(message){
-		    case "1":
-			    return 1;
-			   
-		    case "2":
-			    return 2;
-			   
-	            case "3":
-			    return 3;
-			    
-	            case "4":
-			    return 4;
-			    
-	            case "5":
-			    return 5;
-
-	            case "6":
-			    return 6;
-	            
-			    
-		    default:
-			    return 0;
-	    }
-    }
-
 }
